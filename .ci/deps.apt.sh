9759c0c6a80fdcc5cd4ba3ea4d1f9f022c5018c8
Harsh Kumar Bhartiya <ilikegamess@gmail.com>
2018-03-07 01:36:16 +0530

    config.yml: Update CircleCI syntax
    This updates CircleCI syntax from 1.0 to 2.0

--- .ci/deps.apt.sh
+++ .ci/deps.apt.sh
@@ -22,7 +22,7 @@ case $CIRCLE_BUILD_IMAGE in
     echo '#!/bin/sh' | sudo tee /usr/bin/systemd-detect-virt > /dev/null
     sudo chmod a+x /usr/bin/systemd-detect-virt
 
-    # The non-apt go provided by Circle CI is acceptable
+    # The apt go provided by 14.04 is too old
     deps=${deps/golang-go/}
     # Add packages which are already in the precise image
     deps="$deps g++-4.9 libxml2-utils php-cli php7.0-cli php-codesniffer"
