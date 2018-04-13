911d58959c2b72303f1209fdb7caa31818c74ca7
Harsh Kumar Bhartiya <ilikegamess@gmail.com>
2018-04-13 21:27:09 +0530

    Added new cache

--- .ci/deps.coala-bears.sh
+++ .ci/deps.coala-bears.sh
@@ -5,7 +5,7 @@ set -e
 
 # making coala cache the dependencies downloaded upon first run
 echo '' > /tmp/dummy
-
+which coala
 coala --ci -V --bears CheckstyleBear,ScalaLintBear --files /tmp/dummy --no-config --bear-dirs bears
 
 rm /tmp/dummy
