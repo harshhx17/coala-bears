829bf3311a68bd699d5623a5648e1b62169bed1b
Harsh Kumar Bhartiya <ilikegamess@gmail.com>
2018-04-16 14:44:51 +0530

    Remove error skip

--- .ci/tests.sh
+++ .ci/tests.sh
@@ -9,4 +9,4 @@ if [ "$system_os" == "LINUX" ]; then
   args+=('--cov' '--cov-fail-under=100' '--doctest-modules')
 fi
 pwd
-python3 -m pytest "${args[@]}" --error-for-skips
+python3 -m pytest "${args[@]}"
